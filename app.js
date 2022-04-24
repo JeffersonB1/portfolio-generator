5fa8ac88bf9e91eacc47d88b45104f7fe32ba638
shiba inu <jefferson121996@gmail.com>
2022-04-24 08:14:47 -0400

    used inquirer to promt questions

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-// const animalArray = ['dog', 'cat', 'pig'];
-// animalArray.push('cow');
+const inquirer = require('inquirer');
 
-// const personObj = {
-//     name: 'Lenantino',
-//     age: 99
-// };
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
+// const pageHTML = generatePage(name, github);
 
-// console.log(animalArray);
-// console.log(personObj);
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+=1) {
-        console.log(profileDataArr[i]);
-
-    }
-    console.log('==============');
-};
-
-// Is the same as this....
-profileDataArr.forEach((profileItem) => console.log(profileItem));
-
-printProfileData(profileDataArgs);
-
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
