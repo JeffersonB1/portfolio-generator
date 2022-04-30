67b38e7e9ccbe88a5ddb9e38db7275fb2133b20c
shiba inu <jefferson121996@gmail.com>
2022-04-30 16:49:09 -0400

    save changes

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
 
     return inquirer
@@ -50,11 +44,22 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);  
 };
-
+const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    projects: []
+}
 const promptProject = portfolioData => {
     console.log(`
     =================
@@ -138,7 +143,14 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData)
+    
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+
+        console.log('Page created! Check out index.html in this directory to see it!');
+    });
 });
 
 
